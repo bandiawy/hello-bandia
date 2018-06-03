38438cc081ab92882396583674ca7d8b243bb315
汪勇 <wany@wangsu.com>
2018-06-03 16:32:00 +0800

    add main class

---
+++ src/main/java/com/bandia/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bandia;
+
+/**
+ * @author wany
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
